ceebef41fad0c33667703e76ffe0b695196c0e65
lindsay-alexander <DCG@SVCs-MacBook-Pro.local>
2017-01-30 19:09:55 -0500

    1/30/17

--- class15/list-maker/js/app.js
+++ class15/list-maker/js/app.js
@@ -4,7 +4,8 @@
 // 3. New item is appended as a <li> element to the #list
 // 4. After item is added, the text inside #item should clear
 
-// Requirements: Create a separate function, called appendItem, that accepts one argument, item, that is called when #clickme is clicked and appends the <li> to #list
+// Requirements: Create a separate function, called appendItem, that accepts one argument, item, that is called
+// when #clickme is clicked and appends the <li> to #list
 
 // Bonus: Focus on #item after the item is added
 // Itermediate Bonus: If the value of #item is blank, do not append the <li> and alert user
